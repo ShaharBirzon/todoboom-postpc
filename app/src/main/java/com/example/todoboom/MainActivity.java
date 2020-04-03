a661ee38ae01f45d5bdd69128db3440068c00da9
ShaharBirzon <39333878+ShaharBirzon@users.noreply.github.com>
2020-04-03 12:27:54 +0300

    ex1 finished

--- app/src/main/java/com/example/todoboom/MainActivity.java
+++ app/src/main/java/com/example/todoboom/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.todoboom;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onCreateButtonClicked(View v){
+        EditText inputText = findViewById(R.id.input_text);
+        String value = inputText.getText().toString();
+        inputText.getText().clear();
+        TextView changingText = findViewById(R.id.chaging_text_view);
+        changingText.setText(value);
     }
 }
